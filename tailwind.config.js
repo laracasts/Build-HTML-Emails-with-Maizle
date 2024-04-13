b3f4c5a3eaf2fded3c5602c71fd3d3684b595b27
Jeremy McPeak <jwmcpeak@gmail.com>
2024-04-13 00:50:01 -0500

    Episode 04

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,7 @@ module.exports = {
         sans: ['ui-sans-serif', 'system-ui', '-apple-system', '"Segoe UI"', 'sans-serif'],
         serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
         mono: ['ui-monospace', 'Menlo', 'Consolas', 'monospace'],
+        "public-sans": ['Public Sans'],
       },
       fontSize: {
         0: '0',
